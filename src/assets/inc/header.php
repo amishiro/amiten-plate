dd3a84f7ee00eac25a9deeb07fe038bed9a339e3
Ami <amishiro+github@gmail.com>
2020-01-09 15:10:05 +0900

    #6 ベース項目修正

--- src/assets/inc/header.php
+++ src/assets/inc/header.php
@@ -1,7 +1,15 @@
 <div class="layout__wrapper" id="all">
   <header class="layout__header">
-    <h1>
-      <a href="">AmiTemplate-Simple</a>
-    </h1>
+    <div class="header">
+      <div class="header__wrapper">
+        <div class="header__logo">
+          <div class="logo" itemscope itemtype="http://schema.org/Organization">
+            <a itemprop="url" href="<?php echo $url; ?>">
+              AmiTemplate-Simple
+              <!-- <img itemprop="logo" src="<?php echo $img; ?>/logo.png" /> -->
+            </a>
+          </div>
+        </div>
+      </div>
   </header>
   <main class="layout__main">
---
+++ src/column/index.php
@@ -0,0 +1,32 @@
+<?php
+include("../function.php");
+$meta->pageName = "カラムテスト";
+// $meta->pageDesc = "ページ個別のディスクリプションがあれば記載";
+// $meta->pageImg = "ページ個別のogpImageがあれば記載";
+$bodyClass = new BodyClass('home');
+include($inc . "/head.php");
+include($inc . "/header.php");
+?>
+
+<style>
+.ex-box {
+  background-color: red;
+  min-height: 40px;
+}
+</style>
+
+<article class="main">
+  <section>
+    <h2>h2.heading</h2>
+    <p>つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）つれづれなるまゝに、日暮らし（１００文字）</p>
+  </section>
+  <section>
+    <h2>h2.heading</h2>
+    <p>つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）つれづれなるまゝに、日暮らし（１００文字）</p>
+  </section>
+</article>
+
+<?php
+include($inc . "/footer.php");
+include($inc . "/foot.php");
+?>
