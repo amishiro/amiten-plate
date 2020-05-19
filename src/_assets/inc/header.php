9337b200a8828e0b592a0ed3d453248f679ba084
Ami <amishiro@amiten.co.jp>
2020-05-19 16:46:50 +0900

    feat: Burger menu Design

--- src/_assets/inc/header.php
+++ src/_assets/inc/header.php
@@ -2,18 +2,17 @@
   <header class="layout__header">
     <div class="header">
       <div class="header__wrapper">
-        <div class="header__logo">
 
+        <div class="header__logo">
           <div class="logo" itemscope itemtype="http://schema.org/Organization">
             <a itemprop="url" href="<?php echo $url; ?>">
               AmiTemplate-Simple
               <!-- <img itemprop="logo" src="<?php echo $img; ?>/logo.png" /> -->
             </a>
           </div>
-
         </div>
-        <div class="header__gnav">
 
+        <div class="header__gnav">
           <nav class="gnav">
             <ul class="gnav__wrapper" itemscope itemtype="http://www.schema.org/SiteNavigationElement">
               <li class="gnav__item" itemprop="name">
@@ -35,8 +34,14 @@
           </nav>
 
           <nav class="gnav-slide__menu" @click="isSlideNavOpen = !isSlideNavOpen">
-            <span>menu</span>
-            {{ isSlideNavOpen }}
+            <template v-if="!isSlideNavOpen">
+              <i class="material-icons gnav-slide__menu__icon">menu</i>
+              <span class="gnav-slide__menu__text">menu</span>
+            </template>
+            <template v-if="isSlideNavOpen">
+              <i class="material-icons gnav-slide__menu__icon">close</i>
+              <span class="gnav-slide__menu__text">close</span>
+            </template>
           </nav>
 
           <transition name="gnav-slide-bagckground">
