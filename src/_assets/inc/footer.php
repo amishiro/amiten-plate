690d60a222df6498d228e202547d643439acba63
Ami <amishiro@amiten.co.jp>
2020-05-25 14:02:27 +0900

    fix: footer update

--- src/_assets/inc/footer.php
+++ src/_assets/inc/footer.php
@@ -2,11 +2,31 @@
 </main>
 
 <footer class="layout__footer">
-  <a data-scroll href="#" class="page-top"><i class="material-icons">keyboard_arrow_up</i></a>
+  <a href="#" v-scroll-to="'#app'" class="page-top"><i class="material-icons">keyboard_arrow_up</i></a>
   <div class="footer">
+
+    <nav class="footer__nav ">
+      <div class="fnav">
+        <ul class="fnav__wrapper">
+          <li class="fnav__item">
+            <a href="<?php echo $url ?>/" class="fnav__link sub-item">subItem</a>
+          </li>
+          <li class="fnav__item">
+            <a href="<?php echo $url ?>/" class="fnav__link sub-item">subItem</a>
+          </li>
+          <li class="fnav__item">
+            <a href="https://www.google.co.jp/" class="fnav__link company" target="_blank" rel="nofollow">google</a>
+          </li>
+          <li class="fnav__item">
+            <a href="https://github.com/" class="fnav__link privacy" target="_blank" rel="nofollow">github</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <address class="footer__adr">
-      <div class="footadr__copyright">
-        <span class="footadr__mark">&copy;</span> Amiten. All Rights Reserved.
+      <div class="footer__copyright">
+        <span class="footer__copyright-mark">&copy;</span> Amiten. All Rights Reserved.
       </div>
     </address>
   </div>
