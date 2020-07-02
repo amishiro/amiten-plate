87d1d761019b210578a19ac69d0d89db5c380f9b
Ami <amishiro@amiten.co.jp>
2020-07-02 10:55:04 +0900

    fix: http404

--- src/http404.php
+++ src/http404.php
@@ -8,29 +8,31 @@ include($inc . "/head.php");
 include($inc . "/header.php");
 ?>
 
-<header class="main__header">
-  <?php include($inc . "/main-header.php"); ?>
-</header>
+<div class="main">
+  <header class="main__header">
+    <?php include($inc . "/main-header.php"); ?>
+  </header>
 
-<div class="main__breadcrumb">
-  <?php include($inc . "/main-breadcrumb.php"); ?>
+  <div class="main__breadcrumb">
+    <?php include($inc . "/main-breadcrumb.php"); ?>
+  </div>
+
+  <div class="main__content">
+    <section class="main__section">
+      <h2>音速で探しましたが、お探しのページが見つかりませんでした。</h2>
+      <p>
+        お客さまがお探しのページが見つかりませんでした。URLが正しく入力されているかどうか、もう一度ご確認ください。
+        <br>
+        正しく入力してもページが表示されない場合は、ページが移動したか、もしくは掲載期間が終了し削除された可能性がございます。
+      </p>
+      <p>
+        <a href="<?php echo $baseUrl; ?>/" class="u-icon">
+          <?php echo $meta->siteName; ?>トップへ
+        </a>
+      </p>
+    </section>
+  </div>
 </div>
-
-<article class="main__content">
-  <section class="wrapper">
-    <h2>音速で探しましたが、お探しのページが見つかりませんでした。</h2>
-    <p>
-      お客さまがお探しのページが見つかりませんでした。URLが正しく入力されているかどうか、もう一度ご確認ください。
-      <br>
-      正しく入力してもページが表示されない場合は、ページが移動したか、もしくは掲載期間が終了し削除された可能性がございます。
-    </p>
-    <p>
-      <a href="<?php echo $baseUrl; ?>/" class="u-icon">
-        <?php echo $meta->siteName; ?>トップへ
-      </a>
-    </p>
-  </section>
-</article>
 
 <?php
 include($inc . "/footer.php");
