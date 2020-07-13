6b43d6e53f2e6f69986defb2c66a31002cefc516
Ami <amishiro@amiten.co.jp>
2020-07-13 09:59:33 +0900

    feat: add heading component

--- src/assets/_js/components/vendor.js
+++ src/assets/_js/components/vendor.js
@@ -3,6 +3,7 @@ import Vue from 'vue'
 import BaseAssist from './Base/Assist.vue'
 import BaseButton from './Base/button.vue'
 import BaseButtons from './Base/buttons.vue'
+import BaseHeading from './Base/Heading.vue'
 import slideNav from './layouts/slide-nav.js'
 import objectFitImages from './layouts/object-fit-images.js'
 
@@ -11,6 +12,7 @@ Vue.mixin({
     BaseAssist,
     BaseButton,
     BaseButtons,
+    BaseHeading,
   },
 })
 Vue.mixin(slideNav)
