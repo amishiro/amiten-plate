8205240992547cfab1efad540b8e45cece57e323
Ami <amishiro@amiten.co.jp>
2021-01-06 11:57:52 +0900

    fix: update design

--- src/assets/inc/header.php
+++ src/assets/inc/header.php
@@ -5,7 +5,16 @@
     <div class="l-header__wrapper">
       <div class="l-header__logo" itemscope itemtype="http://schema.org/Organization">
         <a itemprop="url" href="<?php echo $url; ?>" class="l-header__logo-link">
-          amiten-plate
+          <svg xmlns="http://www.w3.org/2000/svg" width="101" height="30" viewBox="0 0 101 30">
+            <text id="HAGURUMA" transform="translate(7 26)" fill="#212121" font-size="16" font-family="Raleway">
+              <tspan x="0" y="0">HAGURUMA</tspan>
+            </text>
+            <text id="YMMK" transform="translate(7 10)" fill="#212121" font-size="11" font-family="Raleway" font-weight="700">
+              <tspan x="0" y="0">YMMK</tspan>
+            </text>
+            <line id="線_9" data-name="線 9" y2="25" transform="translate(0.5 0.5)" fill="none" stroke="#000" stroke-width="1" />
+          </svg>
+
           <!-- <img itemprop="logo" src="assets/img/logo.png" /> -->
         </a>
       </div>
--- src/index.php
+++ src/index.php
@@ -18,20 +18,52 @@ include($inc . "header.php");
 
   <!-- contents -->
   <section class="u-wrapper">
-    <article>
-      <h2>h2.heading</h2>
-      <p>つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）つれづれなるまゝに、日暮らし（１００文字）</p>
-    </article>
-
-    <article>
-      <h2>h2.heading</h2>
-      <p>つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）つれづれなるまゝに、日暮らし（１００文字）</p>
-    </article>
-
-    <article>
-      <h2>h2.heading</h2>
-      <p>つれづれなるまゝに、日暮らし、硯にむかひて、心にうつりゆくよしなし事を、そこはかとなく書きつくれば、あやしうこそものぐるほしけれ。（Wikipediaより）つれづれなるまゝに、日暮らし（１００文字）</p>
-    </article>
+    <div class=''>
+      <h1><a name="title--歯車" class="md-header-anchor"></a><span>title = 歯車</span></h1>
+      <h2><a name="header" class="md-header-anchor"></a><span>header</span></h2>
+      <h3><a name="ブログの紹介" class="md-header-anchor"></a><span>ブログの紹介</span></h3>
+      <ul>
+        <li><span>物書きが東京近辺を散策するブログ</span></li>
+        <li><span>主にグルメ中心</span></li>
+        <li><span>/* articlesの中に最初の記事があってそこにリンク貼るとリアル */</span></li>
+      </ul>
+      <h3><a name="著者紹介" class="md-header-anchor"></a><span>著者紹介</span></h3>
+      <ul>
+        <li><span>芥川龍之介（ペンネーム）</span></li>
+        <li><span>/* articlesの中に一本紹介記事書いてリンク貼る */</span></li>
+      </ul>
+      <h2><a name="sidebar" class="md-header-anchor"></a><span>sidebar</span></h2>
+      <h3><a name="categories-of-contents" class="md-header-anchor"></a><span>categories of contents</span></h3>
+      <ul>
+        <li><span>カフェ</span></li>
+        <li><span>甘味</span></li>
+        <li><span>西洋食</span></li>
+      </ul>
+      <h3><a name="map-plugin" class="md-header-anchor"></a><span>Map Plugin</span></h3>
+      <p><span>/* 記事で紹介した店を地図プラグインでマッピングする */</span></p>
+      <h2><a name="main" class="md-header-anchor"></a><span>main</span></h2>
+      <h3><a name="articles" class="md-header-anchor"></a><span>[articles]</span></h3>
+      <p><span>/* article = API叩いて取ってくる記事コンテンツ */</span></p>
+      <h4><a name="articletitle--ブログ始めました-自己紹介-お気に入りの店が-" class="md-header-anchor"></a><span>article.title = [ブログ始めました, 自己紹介, お気に入りの店が…, ...]</span></h4>
+      <h4><a name="articlemain--" class="md-header-anchor"></a><span>article.main = [...]</span></h4>
+      <p>&nbsp;</p>
+      <p><span>～以下、実例～</span></p>
+      <p>&nbsp;</p>
+      <h5><a name="ブログ始めました" class="md-header-anchor"></a><span>ブログ始めました</span></h5>
+      <p><span>1927年にもなって今更ですが、この頃の都心における文明開化はめざましいもので、自分も一つ日常を書き残そうかとブログを始めてみました。</span></p>
+      <p><span>仕事の話から離れ、主に日常やグルメ等のネタを遺していけたらなと考えています。</span></p>
+      <p>&nbsp;</p>
+      <h5><a name="自己紹介" class="md-header-anchor"></a><span>自己紹介</span></h5>
+      <p><span>芥川龍之介と申します。東大英文科を出たあと、翻訳・教職等を経験。</span></p>
+      <p><span>今はぽつぽつライターやってます。神奈川在住。</span></p>
+      <h2><a name="footer" class="md-header-anchor"></a><span>footer</span></h2>
+      <ul>
+        <li><span>コピーライト = 芥川龍之介</span></li>
+        <li><span>簡易連絡先 = </span></li>
+        <li><span>人気の記事リンク</span></li>
+        <li><span>本当のコピーライト = ryo ymmk</span></li>
+      </ul>
+    </div>
   </section>
 </div>
 
